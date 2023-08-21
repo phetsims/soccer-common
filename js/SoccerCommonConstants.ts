4c49bdb38146cdaa10eb569d462192d3f16f15ac
pixelzoom <cmalley@pixelzoom.com>
2023-08-21 10:17:02 -0400

    https://github.com/phetsims/center-and-variability/issues/479

--- js/SoccerCommonConstants.ts
+++ js/SoccerCommonConstants.ts
@@ -11,7 +11,7 @@ import soccerCommon from './soccerCommon.js';
 const SoccerCommonConstants = {
   GRAVITY: -9.8, // in meters/second^2
   SOCCER_BALL_RADIUS: 0.3, // in meters
-  SOCCER_BALL_OVERLAP: 0.01
+  SOCCER_BALL_OVERLAP: 0.01 //REVIEW also in meters, or is this a percentage?
 };
 
 soccerCommon.register( 'SoccerCommonConstants', SoccerCommonConstants );
