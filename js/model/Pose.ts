495f6df450b00a84d30685a26cae67d166ca47a1
Sam Reid <reids@colorado.edu>
2023-08-23 08:43:49 -0600

    Whitespace

--- js/model/Pose.ts
+++ js/model/Pose.ts
@@ -8,7 +8,6 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-
 import Enumeration from '../../../phet-core/js/Enumeration.js';
 import EnumerationValue from '../../../phet-core/js/EnumerationValue.js';
 import soccerCommon from '../soccerCommon.js';
