2236a781d9d9d4416e529418fe2b7d9e7c81219f
Sam Reid <reids@colorado.edu>
2023-08-31 13:32:13 -0600

    Create individual KickerCharacterSet instances, see https://github.com/phetsims/center-and-variability/issues/425

---
+++ js/view/KickerCharacterSetUSA.ts
@@ -0,0 +1,179 @@
+// Copyright 2023, University of Colorado Boulder
+
+import KickerCharacterSet from './KickerCharacterSet.js';
+import kicker1_set1_headshot_TEMP_png from '../../images/kicker1_set1_headshot_TEMP_png.js';
+import player01Standing_png from '../../images/usa/player01Standing_png.js';
+import player01PoisedToKick_png from '../../images/usa/player01PoisedToKick_png.js';
+import player01Kicking_png from '../../images/usa/player01Kicking_png.js';
+import player02Standing_png from '../../images/usa/player02Standing_png.js';
+import player02PoisedToKick_png from '../../images/usa/player02PoisedToKick_png.js';
+import player02Kicking_png from '../../images/usa/player02Kicking_png.js';
+import player03Standing_png from '../../images/usa/player03Standing_png.js';
+import player03PoisedToKick_png from '../../images/usa/player03PoisedToKick_png.js';
+import player03Kicking_png from '../../images/usa/player03Kicking_png.js';
+import player04Standing_png from '../../images/usa/player04Standing_png.js';
+import player04PoisedToKick_png from '../../images/usa/player04PoisedToKick_png.js';
+import player04Kicking_png from '../../images/usa/player04Kicking_png.js';
+import player05Standing_png from '../../images/usa/player05Standing_png.js';
+import player05PoisedToKick_png from '../../images/usa/player05PoisedToKick_png.js';
+import player05Kicking_png from '../../images/usa/player05Kicking_png.js';
+import player06Standing_png from '../../images/usa/player06Standing_png.js';
+import player06PoisedToKick_png from '../../images/usa/player06PoisedToKick_png.js';
+import player06Kicking_png from '../../images/usa/player06Kicking_png.js';
+import player07Standing_png from '../../images/usa/player07Standing_png.js';
+import player07PoisedToKick_png from '../../images/usa/player07PoisedToKick_png.js';
+import player07Kicking_png from '../../images/usa/player07Kicking_png.js';
+import player08Standing_png from '../../images/usa/player08Standing_png.js';
+import player08PoisedToKick_png from '../../images/usa/player08PoisedToKick_png.js';
+import player08Kicking_png from '../../images/usa/player08Kicking_png.js';
+import player09Standing_png from '../../images/usa/player09Standing_png.js';
+import player09PoisedToKick_png from '../../images/usa/player09PoisedToKick_png.js';
+import player09Kicking_png from '../../images/usa/player09Kicking_png.js';
+import player10Standing_png from '../../images/usa/player10Standing_png.js';
+import player10PoisedToKick_png from '../../images/usa/player10PoisedToKick_png.js';
+import player10Kicking_png from '../../images/usa/player10Kicking_png.js';
+import player11Standing_png from '../../images/usa/player11Standing_png.js';
+import player11PoisedToKick_png from '../../images/usa/player11PoisedToKick_png.js';
+import player11Kicking_png from '../../images/usa/player11Kicking_png.js';
+import player12Standing_png from '../../images/usa/player12Standing_png.js';
+import player12PoisedToKick_png from '../../images/usa/player12PoisedToKick_png.js';
+import player12Kicking_png from '../../images/usa/player12Kicking_png.js';
+import player13Standing_png from '../../images/usa/player13Standing_png.js';
+import player13PoisedToKick_png from '../../images/usa/player13PoisedToKick_png.js';
+import player13Kicking_png from '../../images/usa/player13Kicking_png.js';
+import player14Standing_png from '../../images/usa/player14Standing_png.js';
+import player14PoisedToKick_png from '../../images/usa/player14PoisedToKick_png.js';
+import player14Kicking_png from '../../images/usa/player14Kicking_png.js';
+import player15Standing_png from '../../images/usa/player15Standing_png.js';
+import player15PoisedToKick_png from '../../images/usa/player15PoisedToKick_png.js';
+import player15Kicking_png from '../../images/usa/player15Kicking_png.js';
+import variabilityPlayer01Standing_png from '../../images/usa/variabilityPlayer01Standing_png.js';
+import variabilityPlayer01PoisedToKick_png from '../../images/usa/variabilityPlayer01PoisedToKick_png.js';
+import variabilityPlayer01Kicking_png from '../../images/usa/variabilityPlayer01Kicking_png.js';
+import variabilityPlayer02Standing_png from '../../images/usa/variabilityPlayer02Standing_png.js';
+import variabilityPlayer02PoisedToKick_png from '../../images/usa/variabilityPlayer02PoisedToKick_png.js';
+import variabilityPlayer02Kicking_png from '../../images/usa/variabilityPlayer02Kicking_png.js';
+import variabilityPlayer03Standing_png from '../../images/usa/variabilityPlayer03Standing_png.js';
+import variabilityPlayer03PoisedToKick_png from '../../images/usa/variabilityPlayer03PoisedToKick_png.js';
+import variabilityPlayer03Kicking_png from '../../images/usa/variabilityPlayer03Kicking_png.js';
+import variabilityPlayer04Standing_png from '../../images/usa/variabilityPlayer04Standing_png.js';
+import variabilityPlayer04PoisedToKick_png from '../../images/usa/variabilityPlayer04PoisedToKick_png.js';
+import variabilityPlayer04Kicking_png from '../../images/usa/variabilityPlayer04Kicking_png.js';
+import SoccerCommonStrings from '../SoccerCommonStrings.js';
+
+/**
+ * Artwork representing characters from the United States of America.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+const unitedStatesOfAmericaStringProperty = SoccerCommonStrings.characterSet.unitedStatesOfAmericaStringProperty;
+
+const KickerCharacterSetUSA = new KickerCharacterSet(
+  kicker1_set1_headshot_TEMP_png,
+  unitedStatesOfAmericaStringProperty,
+
+  // Unnumbered Kickers
+  [
+    {
+      standing: player01Standing_png,
+      poisedToKick: player01PoisedToKick_png,
+      kicking: player01Kicking_png
+    },
+    {
+      standing: player02Standing_png,
+      poisedToKick: player02PoisedToKick_png,
+      kicking: player02Kicking_png
+    },
+    {
+      standing: player03Standing_png,
+      poisedToKick: player03PoisedToKick_png,
+      kicking: player03Kicking_png
+    },
+    {
+      standing: player04Standing_png,
+      poisedToKick: player04PoisedToKick_png,
+      kicking: player04Kicking_png
+    },
+    {
+      standing: player05Standing_png,
+      poisedToKick: player05PoisedToKick_png,
+      kicking: player05Kicking_png
+    },
+    {
+      standing: player06Standing_png,
+      poisedToKick: player06PoisedToKick_png,
+      kicking: player06Kicking_png
+    },
+    {
+      standing: player07Standing_png,
+      poisedToKick: player07PoisedToKick_png,
+      kicking: player07Kicking_png
+    },
+    {
+      standing: player08Standing_png,
+      poisedToKick: player08PoisedToKick_png,
+      kicking: player08Kicking_png
+    },
+    {
+      standing: player09Standing_png,
+      poisedToKick: player09PoisedToKick_png,
+      kicking: player09Kicking_png
+    },
+    {
+      standing: player10Standing_png,
+      poisedToKick: player10PoisedToKick_png,
+      kicking: player10Kicking_png
+    },
+    {
+      standing: player11Standing_png,
+      poisedToKick: player11PoisedToKick_png,
+      kicking: player11Kicking_png
+    },
+    {
+      standing: player12Standing_png,
+      poisedToKick: player12PoisedToKick_png,
+      kicking: player12Kicking_png
+    },
+    {
+      standing: player13Standing_png,
+      poisedToKick: player13PoisedToKick_png,
+      kicking: player13Kicking_png
+    },
+    {
+      standing: player14Standing_png,
+      poisedToKick: player14PoisedToKick_png,
+      kicking: player14Kicking_png
+    },
+    {
+      standing: player15Standing_png,
+      poisedToKick: player15PoisedToKick_png,
+      kicking: player15Kicking_png
+    }
+  ],
+
+  // Numbered Kickers
+  [
+    {
+      standing: variabilityPlayer01Standing_png,
+      poisedToKick: variabilityPlayer01PoisedToKick_png,
+      kicking: variabilityPlayer01Kicking_png
+    },
+    {
+      standing: variabilityPlayer02Standing_png,
+      poisedToKick: variabilityPlayer02PoisedToKick_png,
+      kicking: variabilityPlayer02Kicking_png
+    },
+    {
+      standing: variabilityPlayer03Standing_png,
+      poisedToKick: variabilityPlayer03PoisedToKick_png,
+      kicking: variabilityPlayer03Kicking_png
+    },
+    {
+      standing: variabilityPlayer04Standing_png,
+      poisedToKick: variabilityPlayer04PoisedToKick_png,
+      kicking: variabilityPlayer04Kicking_png
+    }
+  ]
+);
+
+export default KickerCharacterSetUSA;
