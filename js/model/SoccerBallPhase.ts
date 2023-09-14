3af4562703977834676cb98feecb678741d78172
matthew-blackman <mabl4992@colorado.edu>
2023-09-14 11:06:32 -0400

    Add documentation to SoccerBallPhase - see https://github.com/phetsims/center-and-variability/issues/549

--- js/model/SoccerBallPhase.ts
+++ js/model/SoccerBallPhase.ts
@@ -11,11 +11,22 @@ import Enumeration from '../../../phet-core/js/Enumeration.js';
 import EnumerationValue from '../../../phet-core/js/EnumerationValue.js';
 import soccerCommon from '../soccerCommon.js';
 
+// Represents the phases of a soccer ball's lifestyle, from being first initialized to reaching its resting position on the field
 export class SoccerBallPhase extends EnumerationValue {
+
+  // The phase in which a soccer ball has not yet been displayed on the field
   public static readonly INACTIVE = new SoccerBallPhase();
+
+  // The phase in which a soccer is displayed in front of the player, but has not yet been kicked
   public static readonly READY = new SoccerBallPhase();
+
+  // The phase after a soccer ball has been kicked, but before it has landed
   public static readonly FLYING = new SoccerBallPhase();
+
+  // The phase after a soccer ball has landed, but before it has finished animating to the top of the stack
   public static readonly STACKING = new SoccerBallPhase();
+
+  // The phase after a soccer ball has finished animating to the top of the stack
   public static readonly STACKED = new SoccerBallPhase();
 
   public static readonly enumeration = new Enumeration( SoccerBallPhase );
