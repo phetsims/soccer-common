e4a8d604323d33040882cef9f82215c014e5b7dd
pixelzoom <cmalley@pixelzoom.com>
2024-03-11 11:19:02 -0600

    rename 'usa' image files, https://github.com/phetsims/soccer-common/issues/13, https://github.com/phetsims/joist/issues/953

--- js/view/KickerPortrayalUSA.ts
+++ js/view/KickerPortrayalUSA.ts
@@ -1,63 +1,63 @@
 // Copyright 2023-2024, University of Colorado Boulder
 
 import KickerPortrayal from './KickerPortrayal.js';
-import player01Standing_svg from '../../images/usa/player01Standing_svg.js';
-import player01PoisedToKick_svg from '../../images/usa/player01PoisedToKick_svg.js';
-import player01Kicking_svg from '../../images/usa/player01Kicking_svg.js';
-import player02Standing_svg from '../../images/usa/player02Standing_svg.js';
-import player02PoisedToKick_svg from '../../images/usa/player02PoisedToKick_svg.js';
-import player02Kicking_svg from '../../images/usa/player02Kicking_svg.js';
-import player03Standing_svg from '../../images/usa/player03Standing_svg.js';
-import player03PoisedToKick_svg from '../../images/usa/player03PoisedToKick_svg.js';
-import player03Kicking_svg from '../../images/usa/player03Kicking_svg.js';
-import player04Standing_svg from '../../images/usa/player04Standing_svg.js';
-import player04PoisedToKick_svg from '../../images/usa/player04PoisedToKick_svg.js';
-import player04Kicking_svg from '../../images/usa/player04Kicking_svg.js';
-import player05Standing_svg from '../../images/usa/player05Standing_svg.js';
-import player05PoisedToKick_svg from '../../images/usa/player05PoisedToKick_svg.js';
-import player05Kicking_svg from '../../images/usa/player05Kicking_svg.js';
-import player06Standing_svg from '../../images/usa/player06Standing_svg.js';
-import player06PoisedToKick_svg from '../../images/usa/player06PoisedToKick_svg.js';
-import player06Kicking_svg from '../../images/usa/player06Kicking_svg.js';
-import player07Standing_svg from '../../images/usa/player07Standing_svg.js';
-import player07PoisedToKick_svg from '../../images/usa/player07PoisedToKick_svg.js';
-import player07Kicking_svg from '../../images/usa/player07Kicking_svg.js';
-import player08Standing_svg from '../../images/usa/player08Standing_svg.js';
-import player08PoisedToKick_svg from '../../images/usa/player08PoisedToKick_svg.js';
-import player08Kicking_svg from '../../images/usa/player08Kicking_svg.js';
-import player09Standing_svg from '../../images/usa/player09Standing_svg.js';
-import player09PoisedToKick_svg from '../../images/usa/player09PoisedToKick_svg.js';
-import player09Kicking_svg from '../../images/usa/player09Kicking_svg.js';
-import player10Standing_svg from '../../images/usa/player10Standing_svg.js';
-import player10PoisedToKick_svg from '../../images/usa/player10PoisedToKick_svg.js';
-import player10Kicking_svg from '../../images/usa/player10Kicking_svg.js';
-import player11Standing_svg from '../../images/usa/player11Standing_svg.js';
-import player11PoisedToKick_svg from '../../images/usa/player11PoisedToKick_svg.js';
-import player11Kicking_svg from '../../images/usa/player11Kicking_svg.js';
-import player12Standing_svg from '../../images/usa/player12Standing_svg.js';
-import player12PoisedToKick_svg from '../../images/usa/player12PoisedToKick_svg.js';
-import player12Kicking_svg from '../../images/usa/player12Kicking_svg.js';
-import player13Standing_svg from '../../images/usa/player13Standing_svg.js';
-import player13PoisedToKick_svg from '../../images/usa/player13PoisedToKick_svg.js';
-import player13Kicking_svg from '../../images/usa/player13Kicking_svg.js';
-import player14Standing_svg from '../../images/usa/player14Standing_svg.js';
-import player14PoisedToKick_svg from '../../images/usa/player14PoisedToKick_svg.js';
-import player14Kicking_svg from '../../images/usa/player14Kicking_svg.js';
-import player15Standing_svg from '../../images/usa/player15Standing_svg.js';
-import player15PoisedToKick_svg from '../../images/usa/player15PoisedToKick_svg.js';
-import player15Kicking_svg from '../../images/usa/player15Kicking_svg.js';
-import variabilityPlayer01Standing_svg from '../../images/usa/variabilityPlayer01Standing_svg.js';
-import variabilityPlayer01PoisedToKick_svg from '../../images/usa/variabilityPlayer01PoisedToKick_svg.js';
-import variabilityPlayer01Kicking_svg from '../../images/usa/variabilityPlayer01Kicking_svg.js';
-import variabilityPlayer02Standing_svg from '../../images/usa/variabilityPlayer02Standing_svg.js';
-import variabilityPlayer02PoisedToKick_svg from '../../images/usa/variabilityPlayer02PoisedToKick_svg.js';
-import variabilityPlayer02Kicking_svg from '../../images/usa/variabilityPlayer02Kicking_svg.js';
-import variabilityPlayer03Standing_svg from '../../images/usa/variabilityPlayer03Standing_svg.js';
-import variabilityPlayer03PoisedToKick_svg from '../../images/usa/variabilityPlayer03PoisedToKick_svg.js';
-import variabilityPlayer03Kicking_svg from '../../images/usa/variabilityPlayer03Kicking_svg.js';
-import variabilityPlayer04Standing_svg from '../../images/usa/variabilityPlayer04Standing_svg.js';
-import variabilityPlayer04PoisedToKick_svg from '../../images/usa/variabilityPlayer04PoisedToKick_svg.js';
-import variabilityPlayer04Kicking_svg from '../../images/usa/variabilityPlayer04Kicking_svg.js';
+import usaPlayer01Standing_svg from '../../images/usa/usaPlayer01Standing_svg.js';
+import usaPlayer01PoisedToKick_svg from '../../images/usa/usaPlayer01PoisedToKick_svg.js';
+import usaPlayer01Kicking_svg from '../../images/usa/usaPlayer01Kicking_svg.js';
+import usaPlayer02Standing_svg from '../../images/usa/usaPlayer02Standing_svg.js';
+import usaPlayer02PoisedToKick_svg from '../../images/usa/usaPlayer02PoisedToKick_svg.js';
+import usaPlayer02Kicking_svg from '../../images/usa/usaPlayer02Kicking_svg.js';
+import usaPlayer03Standing_svg from '../../images/usa/usaPlayer03Standing_svg.js';
+import usaPlayer03PoisedToKick_svg from '../../images/usa/usaPlayer03PoisedToKick_svg.js';
+import usaPlayer03Kicking_svg from '../../images/usa/usaPlayer03Kicking_svg.js';
+import usaPlayer04Standing_svg from '../../images/usa/usaPlayer04Standing_svg.js';
+import usaPlayer04PoisedToKick_svg from '../../images/usa/usaPlayer04PoisedToKick_svg.js';
+import usaPlayer04Kicking_svg from '../../images/usa/usaPlayer04Kicking_svg.js';
+import usaPlayer05Standing_svg from '../../images/usa/usaPlayer05Standing_svg.js';
+import usaPlayer05PoisedToKick_svg from '../../images/usa/usaPlayer05PoisedToKick_svg.js';
+import usaPlayer05Kicking_svg from '../../images/usa/usaPlayer05Kicking_svg.js';
+import usaPlayer06Standing_svg from '../../images/usa/usaPlayer06Standing_svg.js';
+import usaPlayer06PoisedToKick_svg from '../../images/usa/usaPlayer06PoisedToKick_svg.js';
+import usaPlayer06Kicking_svg from '../../images/usa/usaPlayer06Kicking_svg.js';
+import usaPlayer07Standing_svg from '../../images/usa/usaPlayer07Standing_svg.js';
+import usaPlayer07PoisedToKick_svg from '../../images/usa/usaPlayer07PoisedToKick_svg.js';
+import usaPlayer07Kicking_svg from '../../images/usa/usaPlayer07Kicking_svg.js';
+import usaPlayer08Standing_svg from '../../images/usa/usaPlayer08Standing_svg.js';
+import usaPlayer08PoisedToKick_svg from '../../images/usa/usaPlayer08PoisedToKick_svg.js';
+import usaPlayer08Kicking_svg from '../../images/usa/usaPlayer08Kicking_svg.js';
+import usaPlayer09Standing_svg from '../../images/usa/usaPlayer09Standing_svg.js';
+import usaPlayer09PoisedToKick_svg from '../../images/usa/usaPlayer09PoisedToKick_svg.js';
+import usaPlayer09Kicking_svg from '../../images/usa/usaPlayer09Kicking_svg.js';
+import usaPlayer10Standing_svg from '../../images/usa/usaPlayer10Standing_svg.js';
+import usaPlayer10PoisedToKick_svg from '../../images/usa/usaPlayer10PoisedToKick_svg.js';
+import usaPlayer10Kicking_svg from '../../images/usa/usaPlayer10Kicking_svg.js';
+import usaPlayer11Standing_svg from '../../images/usa/usaPlayer11Standing_svg.js';
+import usaPlayer11PoisedToKick_svg from '../../images/usa/usaPlayer11PoisedToKick_svg.js';
+import usaPlayer11Kicking_svg from '../../images/usa/usaPlayer11Kicking_svg.js';
+import usaPlayer12Standing_svg from '../../images/usa/usaPlayer12Standing_svg.js';
+import usaPlayer12PoisedToKick_svg from '../../images/usa/usaPlayer12PoisedToKick_svg.js';
+import usaPlayer12Kicking_svg from '../../images/usa/usaPlayer12Kicking_svg.js';
+import usaPlayer13Standing_svg from '../../images/usa/usaPlayer13Standing_svg.js';
+import usaPlayer13PoisedToKick_svg from '../../images/usa/usaPlayer13PoisedToKick_svg.js';
+import usaPlayer13Kicking_svg from '../../images/usa/usaPlayer13Kicking_svg.js';
+import usaPlayer14Standing_svg from '../../images/usa/usaPlayer14Standing_svg.js';
+import usaPlayer14PoisedToKick_svg from '../../images/usa/usaPlayer14PoisedToKick_svg.js';
+import usaPlayer14Kicking_svg from '../../images/usa/usaPlayer14Kicking_svg.js';
+import usaPlayer15Standing_svg from '../../images/usa/usaPlayer15Standing_svg.js';
+import usaPlayer15PoisedToKick_svg from '../../images/usa/usaPlayer15PoisedToKick_svg.js';
+import usaPlayer15Kicking_svg from '../../images/usa/usaPlayer15Kicking_svg.js';
+import usaVariabilityPlayer01Standing_svg from '../../images/usa/usaVariabilityPlayer01Standing_svg.js';
+import usaVariabilityPlayer01PoisedToKick_svg from '../../images/usa/usaVariabilityPlayer01PoisedToKick_svg.js';
+import usaVariabilityPlayer01Kicking_svg from '../../images/usa/usaVariabilityPlayer01Kicking_svg.js';
+import usaVariabilityPlayer02Standing_svg from '../../images/usa/usaVariabilityPlayer02Standing_svg.js';
+import usaVariabilityPlayer02PoisedToKick_svg from '../../images/usa/usaVariabilityPlayer02PoisedToKick_svg.js';
+import usaVariabilityPlayer02Kicking_svg from '../../images/usa/usaVariabilityPlayer02Kicking_svg.js';
+import usaVariabilityPlayer03Standing_svg from '../../images/usa/usaVariabilityPlayer03Standing_svg.js';
+import usaVariabilityPlayer03PoisedToKick_svg from '../../images/usa/usaVariabilityPlayer03PoisedToKick_svg.js';
+import usaVariabilityPlayer03Kicking_svg from '../../images/usa/usaVariabilityPlayer03Kicking_svg.js';
+import usaVariabilityPlayer04Standing_svg from '../../images/usa/usaVariabilityPlayer04Standing_svg.js';
+import usaVariabilityPlayer04PoisedToKick_svg from '../../images/usa/usaVariabilityPlayer04PoisedToKick_svg.js';
+import usaVariabilityPlayer04Kicking_svg from '../../images/usa/usaVariabilityPlayer04Kicking_svg.js';
 import JoistStrings from '../../../joist/js/JoistStrings.js';
 import { USA_REGION_AND_CULTURE_ID } from '../../../joist/js/preferences/RegionAndCulturePortrayal.js';
 
@@ -74,103 +74,103 @@ const KickerPortrayalUSA = new KickerPortrayal(
   // Unnumbered Kickers
   [
     {
-      standing: player01Standing_svg,
-      poisedToKick: player01PoisedToKick_svg,
-      kicking: player01Kicking_svg
+      standing: usaPlayer01Standing_svg,
+      poisedToKick: usaPlayer01PoisedToKick_svg,
+      kicking: usaPlayer01Kicking_svg
     },
     {
-      standing: player02Standing_svg,
-      poisedToKick: player02PoisedToKick_svg,
-      kicking: player02Kicking_svg
+      standing: usaPlayer02Standing_svg,
+      poisedToKick: usaPlayer02PoisedToKick_svg,
+      kicking: usaPlayer02Kicking_svg
     },
     {
-      standing: player03Standing_svg,
-      poisedToKick: player03PoisedToKick_svg,
-      kicking: player03Kicking_svg
+      standing: usaPlayer03Standing_svg,
+      poisedToKick: usaPlayer03PoisedToKick_svg,
+      kicking: usaPlayer03Kicking_svg
     },
     {
-      standing: player04Standing_svg,
-      poisedToKick: player04PoisedToKick_svg,
-      kicking: player04Kicking_svg
+      standing: usaPlayer04Standing_svg,
+      poisedToKick: usaPlayer04PoisedToKick_svg,
+      kicking: usaPlayer04Kicking_svg
     },
     {
-      standing: player05Standing_svg,
-      poisedToKick: player05PoisedToKick_svg,
-      kicking: player05Kicking_svg
+      standing: usaPlayer05Standing_svg,
+      poisedToKick: usaPlayer05PoisedToKick_svg,
+      kicking: usaPlayer05Kicking_svg
     },
     {
-      standing: player06Standing_svg,
-      poisedToKick: player06PoisedToKick_svg,
-      kicking: player06Kicking_svg
+      standing: usaPlayer06Standing_svg,
+      poisedToKick: usaPlayer06PoisedToKick_svg,
+      kicking: usaPlayer06Kicking_svg
     },
     {
-      standing: player07Standing_svg,
-      poisedToKick: player07PoisedToKick_svg,
-      kicking: player07Kicking_svg
+      standing: usaPlayer07Standing_svg,
+      poisedToKick: usaPlayer07PoisedToKick_svg,
+      kicking: usaPlayer07Kicking_svg
     },
     {
-      standing: player08Standing_svg,
-      poisedToKick: player08PoisedToKick_svg,
-      kicking: player08Kicking_svg
+      standing: usaPlayer08Standing_svg,
+      poisedToKick: usaPlayer08PoisedToKick_svg,
+      kicking: usaPlayer08Kicking_svg
     },
     {
-      standing: player09Standing_svg,
-      poisedToKick: player09PoisedToKick_svg,
-      kicking: player09Kicking_svg
+      standing: usaPlayer09Standing_svg,
+      poisedToKick: usaPlayer09PoisedToKick_svg,
+      kicking: usaPlayer09Kicking_svg
     },
     {
-      standing: player10Standing_svg,
-      poisedToKick: player10PoisedToKick_svg,
-      kicking: player10Kicking_svg
+      standing: usaPlayer10Standing_svg,
+      poisedToKick: usaPlayer10PoisedToKick_svg,
+      kicking: usaPlayer10Kicking_svg
     },
     {
-      standing: player11Standing_svg,
-      poisedToKick: player11PoisedToKick_svg,
-      kicking: player11Kicking_svg
+      standing: usaPlayer11Standing_svg,
+      poisedToKick: usaPlayer11PoisedToKick_svg,
+      kicking: usaPlayer11Kicking_svg
     },
     {
-      standing: player12Standing_svg,
-      poisedToKick: player12PoisedToKick_svg,
-      kicking: player12Kicking_svg
+      standing: usaPlayer12Standing_svg,
+      poisedToKick: usaPlayer12PoisedToKick_svg,
+      kicking: usaPlayer12Kicking_svg
     },
     {
-      standing: player13Standing_svg,
-      poisedToKick: player13PoisedToKick_svg,
-      kicking: player13Kicking_svg
+      standing: usaPlayer13Standing_svg,
+      poisedToKick: usaPlayer13PoisedToKick_svg,
+      kicking: usaPlayer13Kicking_svg
     },
     {
-      standing: player14Standing_svg,
-      poisedToKick: player14PoisedToKick_svg,
-      kicking: player14Kicking_svg
+      standing: usaPlayer14Standing_svg,
+      poisedToKick: usaPlayer14PoisedToKick_svg,
+      kicking: usaPlayer14Kicking_svg
     },
     {
-      standing: player15Standing_svg,
-      poisedToKick: player15PoisedToKick_svg,
-      kicking: player15Kicking_svg
+      standing: usaPlayer15Standing_svg,
+      poisedToKick: usaPlayer15PoisedToKick_svg,
+      kicking: usaPlayer15Kicking_svg
     }
   ],
 
   // Numbered Kickers
   [
     {
-      standing: variabilityPlayer01Standing_svg,
-      poisedToKick: variabilityPlayer01PoisedToKick_svg,
-      kicking: variabilityPlayer01Kicking_svg
+      standing: usaVariabilityPlayer01Standing_svg,
+      poisedToKick: usaVariabilityPlayer01PoisedToKick_svg,
+      kicking: usaVariabilityPlayer01Kicking_svg
     },
     {
-      standing: variabilityPlayer02Standing_svg,
-      poisedToKick: variabilityPlayer02PoisedToKick_svg,
-      kicking: variabilityPlayer02Kicking_svg
+      standing: usaVariabilityPlayer02Standing_svg,
+      poisedToKick: usaVariabilityPlayer02PoisedToKick_svg,
+      kicking: usaVariabilityPlayer02Kicking_svg
     },
     {
-      standing: variabilityPlayer03Standing_svg,
-      poisedToKick: variabilityPlayer03PoisedToKick_svg,
-      kicking: variabilityPlayer03Kicking_svg
+      standing: usaVariabilityPlayer03Standing_svg,
+      poisedToKick: usaVariabilityPlayer03PoisedToKick_svg,
+      kicking: usaVariabilityPlayer03Kicking_svg
     },
     {
-      standing: variabilityPlayer04Standing_svg,
-      poisedToKick: variabilityPlayer04PoisedToKick_svg,
-      kicking: variabilityPlayer04Kicking_svg
+      standing: usaVariabilityPlayer04Standing_svg,
+      poisedToKick: usaVariabilityPlayer04PoisedToKick_svg,
+      kicking: usaVariabilityPlayer04Kicking_svg
     }
   ],
   USA_REGION_AND_CULTURE_ID );
