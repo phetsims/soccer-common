e27823b3c8627a3ed8f31737c53f58cf5763457d
pixelzoom <cmalley@pixelzoom.com>
2024-03-12 14:04:09 -0600

    integrate new SVG images for africa and africaModest, https://github.com/phetsims/soccer-common/issues/13

--- js/view/KickerPortrayalAfrica.ts
+++ js/view/KickerPortrayalAfrica.ts
@@ -1,40 +1,46 @@
 // Copyright 2023-2024, University of Colorado Boulder
 
 import KickerPortrayal from './KickerPortrayal.js';
-import africaKicker01Standing_png from '../../images/africa/africaKicker01Standing_png.js';
-import africaKicker01PoisedToKick_png from '../../images/africa/africaKicker01PoisedToKick_png.js';
-import africaKicker01Kicking_png from '../../images/africa/africaKicker01Kicking_png.js';
-import africaKicker02Standing_png from '../../images/africa/africaKicker02Standing_png.js';
-import africaKicker02PoisedToKick_png from '../../images/africa/africaKicker02PoisedToKick_png.js';
-import africaKicker02Kicking_png from '../../images/africa/africaKicker02Kicking_png.js';
-import africaKicker03Standing_png from '../../images/africa/africaKicker03Standing_png.js';
-import africaKicker03PoisedToKick_png from '../../images/africa/africaKicker03PoisedToKick_png.js';
-import africaKicker03Kicking_png from '../../images/africa/africaKicker03Kicking_png.js';
-import africaKicker04Standing_png from '../../images/africa/africaKicker04Standing_png.js';
-import africaKicker04PoisedToKick_png from '../../images/africa/africaKicker04PoisedToKick_png.js';
-import africaKicker04Kicking_png from '../../images/africa/africaKicker04Kicking_png.js';
-import africaKicker07Standing_png from '../../images/africa/africaKicker07Standing_png.js';
-import africaKicker07PoisedToKick_png from '../../images/africa/africaKicker07PoisedToKick_png.js';
-import africaKicker07Kicking_png from '../../images/africa/africaKicker07Kicking_png.js';
-import africaKicker11Standing_png from '../../images/africa/africaKicker11Standing_png.js';
-import africaKicker11PoisedToKick_png from '../../images/africa/africaKicker11PoisedToKick_png.js';
-import africaKicker11Kicking_png from '../../images/africa/africaKicker11Kicking_png.js';
-import africaKicker13Standing_png from '../../images/africa/africaKicker13Standing_png.js';
-import africaKicker13PoisedToKick_png from '../../images/africa/africaKicker13PoisedToKick_png.js';
-import africaKicker13Kicking_png from '../../images/africa/africaKicker13Kicking_png.js';
 import KickerPortrayalUSA from './KickerPortrayalUSA.js';
-import africaVariabilityKicker01Standing_png from '../../images/africa/africaVariabilityKicker01Standing_png.js';
-import africaVariabilityKicker01PoisedToKick_png from '../../images/africa/africaVariabilityKicker01PoisedToKick_png.js';
-import africaVariabilityKicker01Kicking_png from '../../images/africa/africaVariabilityKicker01Kicking_png.js';
-import africaVariabilityKicker02Standing_png from '../../images/africa/africaVariabilityKicker02Standing_png.js';
-import africaVariabilityKicker02PoisedToKick_png from '../../images/africa/africaVariabilityKicker02PoisedToKick_png.js';
-import africaVariabilityKicker02Kicking_png from '../../images/africa/africaVariabilityKicker02Kicking_png.js';
-import africaVariabilityKicker03Standing_png from '../../images/africa/africaVariabilityKicker03Standing_png.js';
-import africaVariabilityKicker03PoisedToKick_png from '../../images/africa/africaVariabilityKicker03PoisedToKick_png.js';
-import africaVariabilityKicker03Kicking_png from '../../images/africa/africaVariabilityKicker03Kicking_png.js';
-import africaVariabilityKicker04Standing_png from '../../images/africa/africaVariabilityKicker04Standing_png.js';
-import africaVariabilityKicker04PoisedToKick_png from '../../images/africa/africaVariabilityKicker04PoisedToKick_png.js';
-import africaVariabilityKicker04Kicking_png from '../../images/africa/africaVariabilityKicker04Kicking_png.js';
+import africaKicker01Standing_svg from '../../images/africa/africaKicker01Standing_svg.js';
+import africaKicker01PoisedToKick_svg from '../../images/africa/africaKicker01PoisedToKick_svg.js';
+import africaKicker01Kicking_svg from '../../images/africa/africaKicker01Kicking_svg.js';
+import africaKicker02Standing_svg from '../../images/africa/africaKicker02Standing_svg.js';
+import africaKicker02PoisedToKick_svg from '../../images/africa/africaKicker02PoisedToKick_svg.js';
+import africaKicker02Kicking_svg from '../../images/africa/africaKicker02Kicking_svg.js';
+import africaKicker03Standing_svg from '../../images/africa/africaKicker03Standing_svg.js';
+import africaKicker03PoisedToKick_svg from '../../images/africa/africaKicker03PoisedToKick_svg.js';
+import africaKicker03Kicking_svg from '../../images/africa/africaKicker03Kicking_svg.js';
+import africaKicker04Standing_svg from '../../images/africa/africaKicker04Standing_svg.js';
+import africaKicker04PoisedToKick_svg from '../../images/africa/africaKicker04PoisedToKick_svg.js';
+import africaKicker04Kicking_svg from '../../images/africa/africaKicker04Kicking_svg.js';
+import africaKicker07Standing_svg from '../../images/africa/africaKicker07Standing_svg.js';
+import africaKicker07PoisedToKick_svg from '../../images/africa/africaKicker07PoisedToKick_svg.js';
+import africaKicker07Kicking_svg from '../../images/africa/africaKicker07Kicking_svg.js';
+import africaKicker11Standing_svg from '../../images/africa/africaKicker11Standing_svg.js';
+import africaKicker11PoisedToKick_svg from '../../images/africa/africaKicker11PoisedToKick_svg.js';
+import africaKicker11Kicking_svg from '../../images/africa/africaKicker11Kicking_svg.js';
+import africaKicker13Standing_svg from '../../images/africa/africaKicker13Standing_svg.js';
+import africaKicker13PoisedToKick_svg from '../../images/africa/africaKicker13PoisedToKick_svg.js';
+import africaKicker13Kicking_svg from '../../images/africa/africaKicker13Kicking_svg.js';
+import africaKicker14Standing_svg from '../../images/africa/africaKicker14Standing_svg.js';
+import africaKicker14PoisedToKick_svg from '../../images/africa/africaKicker14PoisedToKick_svg.js';
+import africaKicker14Kicking_svg from '../../images/africa/africaKicker14Kicking_svg.js';
+import africaKicker15Standing_svg from '../../images/africa/africaKicker15Standing_svg.js';
+import africaKicker15PoisedToKick_svg from '../../images/africa/africaKicker15PoisedToKick_svg.js';
+import africaKicker15Kicking_svg from '../../images/africa/africaKicker15Kicking_svg.js';
+import africaVariabilityKicker01Standing_svg from '../../images/africa/africaVariabilityKicker01Standing_svg.js';
+import africaVariabilityKicker01PoisedToKick_svg from '../../images/africa/africaVariabilityKicker01PoisedToKick_svg.js';
+import africaVariabilityKicker01Kicking_svg from '../../images/africa/africaVariabilityKicker01Kicking_svg.js';
+import africaVariabilityKicker02Standing_svg from '../../images/africa/africaVariabilityKicker02Standing_svg.js';
+import africaVariabilityKicker02PoisedToKick_svg from '../../images/africa/africaVariabilityKicker02PoisedToKick_svg.js';
+import africaVariabilityKicker02Kicking_svg from '../../images/africa/africaVariabilityKicker02Kicking_svg.js';
+import africaVariabilityKicker03Standing_svg from '../../images/africa/africaVariabilityKicker03Standing_svg.js';
+import africaVariabilityKicker03PoisedToKick_svg from '../../images/africa/africaVariabilityKicker03PoisedToKick_svg.js';
+import africaVariabilityKicker03Kicking_svg from '../../images/africa/africaVariabilityKicker03Kicking_svg.js';
+import africaVariabilityKicker04Standing_svg from '../../images/africa/africaVariabilityKicker04Standing_svg.js';
+import africaVariabilityKicker04PoisedToKick_svg from '../../images/africa/africaVariabilityKicker04PoisedToKick_svg.js';
+import africaVariabilityKicker04Kicking_svg from '../../images/africa/africaVariabilityKicker04Kicking_svg.js';
 
 /**
  * Artwork representing characters from Africa.
@@ -48,33 +54,33 @@ const KickerPortrayalAfrica = new KickerPortrayal(
   // Unnumbered Kickers
   [
     {
-      standing: africaKicker01Standing_png,
-      poisedToKick: africaKicker01PoisedToKick_png,
-      kicking: africaKicker01Kicking_png
+      standing: africaKicker01Standing_svg,
+      poisedToKick: africaKicker01PoisedToKick_svg,
+      kicking: africaKicker01Kicking_svg
     },
     {
-      standing: africaKicker02Standing_png,
-      poisedToKick: africaKicker02PoisedToKick_png,
-      kicking: africaKicker02Kicking_png
+      standing: africaKicker02Standing_svg,
+      poisedToKick: africaKicker02PoisedToKick_svg,
+      kicking: africaKicker02Kicking_svg
     },
     {
-      standing: africaKicker03Standing_png,
-      poisedToKick: africaKicker03PoisedToKick_png,
-      kicking: africaKicker03Kicking_png
+      standing: africaKicker03Standing_svg,
+      poisedToKick: africaKicker03PoisedToKick_svg,
+      kicking: africaKicker03Kicking_svg
     },
     {
-      standing: africaKicker04Standing_png,
-      poisedToKick: africaKicker04PoisedToKick_png,
-      kicking: africaKicker04Kicking_png
+      standing: africaKicker04Standing_svg,
+      poisedToKick: africaKicker04PoisedToKick_svg,
+      kicking: africaKicker04Kicking_svg
     },
 
     KickerPortrayalUSA.unnumberedKickerImages[ 8 - 1 ],
     KickerPortrayalUSA.unnumberedKickerImages[ 14 - 1 ],
 
     {
-      standing: africaKicker07Standing_png,
-      poisedToKick: africaKicker07PoisedToKick_png,
-      kicking: africaKicker07Kicking_png
+      standing: africaKicker07Standing_svg,
+      poisedToKick: africaKicker07PoisedToKick_svg,
+      kicking: africaKicker07Kicking_svg
     },
 
     KickerPortrayalUSA.unnumberedKickerImages[ 12 - 1 ],
@@ -82,38 +88,49 @@ const KickerPortrayalAfrica = new KickerPortrayal(
     KickerPortrayalUSA.unnumberedKickerImages[ 2 - 1 ],
 
     {
-      standing: africaKicker11Standing_png,
-      poisedToKick: africaKicker11PoisedToKick_png,
-      kicking: africaKicker11Kicking_png
+      standing: africaKicker11Standing_svg,
+      poisedToKick: africaKicker11PoisedToKick_svg,
+      kicking: africaKicker11Kicking_svg
     },
     KickerPortrayalUSA.unnumberedKickerImages[ 4 - 1 ],
     {
-      standing: africaKicker13Standing_png,
-      poisedToKick: africaKicker13PoisedToKick_png,
-      kicking: africaKicker13Kicking_png
-    } ],
+      standing: africaKicker13Standing_svg,
+      poisedToKick: africaKicker13PoisedToKick_svg,
+      kicking: africaKicker13Kicking_svg
+    },
+    {
+      standing: africaKicker14Standing_svg,
+      poisedToKick: africaKicker14PoisedToKick_svg,
+      kicking: africaKicker14Kicking_svg
+    },
+    {
+      standing: africaKicker15Standing_svg,
+      poisedToKick: africaKicker15PoisedToKick_svg,
+      kicking: africaKicker15Kicking_svg
+    }
+  ],
 
   // Numbered Kickers
   [
     {
-      standing: africaVariabilityKicker01Standing_png,
-      poisedToKick: africaVariabilityKicker01PoisedToKick_png,
-      kicking: africaVariabilityKicker01Kicking_png
+      standing: africaVariabilityKicker01Standing_svg,
+      poisedToKick: africaVariabilityKicker01PoisedToKick_svg,
+      kicking: africaVariabilityKicker01Kicking_svg
     },
     {
-      standing: africaVariabilityKicker02Standing_png,
-      poisedToKick: africaVariabilityKicker02PoisedToKick_png,
-      kicking: africaVariabilityKicker02Kicking_png
+      standing: africaVariabilityKicker02Standing_svg,
+      poisedToKick: africaVariabilityKicker02PoisedToKick_svg,
+      kicking: africaVariabilityKicker02Kicking_svg
     },
     {
-      standing: africaVariabilityKicker03Standing_png,
-      poisedToKick: africaVariabilityKicker03PoisedToKick_png,
-      kicking: africaVariabilityKicker03Kicking_png
+      standing: africaVariabilityKicker03Standing_svg,
+      poisedToKick: africaVariabilityKicker03PoisedToKick_svg,
+      kicking: africaVariabilityKicker03Kicking_svg
     },
     {
-      standing: africaVariabilityKicker04Standing_png,
-      poisedToKick: africaVariabilityKicker04PoisedToKick_png,
-      kicking: africaVariabilityKicker04Kicking_png
+      standing: africaVariabilityKicker04Standing_svg,
+      poisedToKick: africaVariabilityKicker04PoisedToKick_svg,
+      kicking: africaVariabilityKicker04Kicking_svg
     }
   ]
 );
--- js/view/KickerPortrayalAfricaModest.ts
+++ js/view/KickerPortrayalAfricaModest.ts
@@ -1,17 +1,32 @@
 // Copyright 2023-2024, University of Colorado Boulder
 
 import KickerPortrayal from './KickerPortrayal.js';
-import africaModestKicker03Standing_png from '../../images/africaModest/africaModestKicker03Standing_png.js';
-import africaModestKicker03PoisedToKick_png from '../../images/africaModest/africaModestKicker03PoisedToKick_png.js';
-import africaModestKicker03Kicking_png from '../../images/africaModest/africaModestKicker03Kicking_png.js';
-import africaModestKicker06Standing_png from '../../images/africaModest/africaModestKicker06Standing_png.js';
-import africaModestKicker06PoisedToKick_png from '../../images/africaModest/africaModestKicker06PoisedToKick_png.js';
-import africaModestKicker06Kicking_png from '../../images/africaModest/africaModestKicker06Kicking_png.js';
 import KickerPortrayalUSA from './KickerPortrayalUSA.js';
 import KickerPortrayalAfrica from './KickerPortrayalAfrica.js';
-import africaModestVariabilityKicker02Standing_png from '../../images/africaModest/africaModestVariabilityKicker02Standing_png.js';
-import africaModestVariabilityKicker02PoisedToKick_png from '../../images/africaModest/africaModestVariabilityKicker02PoisedToKick_png.js';
-import africaModestVariabilityKicker02Kicking_png from '../../images/africaModest/africaModestVariabilityKicker02Kicking_png.js';
+import africaModestVariabilityKicker02Standing_svg from '../../images/africaModest/africaModestVariabilityKicker02Standing_svg.js';
+import africaModestVariabilityKicker02PoisedToKick_svg from '../../images/africaModest/africaModestVariabilityKicker02PoisedToKick_svg.js';
+import africaModestVariabilityKicker02Kicking_svg from '../../images/africaModest/africaModestVariabilityKicker02Kicking_svg.js';
+import africaModestKicker01Standing_svg from '../../images/africaModest/africaModestKicker01Standing_svg.js';
+import africaModestKicker01PoisedToKick_svg from '../../images/africaModest/africaModestKicker01PoisedToKick_svg.js';
+import africaModestKicker01Kicking_svg from '../../images/africaModest/africaModestKicker01Kicking_svg.js';
+import africaModestKicker02Standing_svg from '../../images/africaModest/africaModestKicker02Standing_svg.js';
+import africaModestKicker02PoisedToKick_svg from '../../images/africaModest/africaModestKicker02PoisedToKick_svg.js';
+import africaModestKicker02Kicking_svg from '../../images/africaModest/africaModestKicker02Kicking_svg.js';
+import africaModestKicker03Standing_svg from '../../images/africaModest/africaModestKicker03Standing_svg.js';
+import africaModestKicker03PoisedToKick_svg from '../../images/africaModest/africaModestKicker03PoisedToKick_svg.js';
+import africaModestKicker03Kicking_svg from '../../images/africaModest/africaModestKicker03Kicking_svg.js';
+import africaModestKicker05Standing_svg from '../../images/africaModest/africaModestKicker05Standing_svg.js';
+import africaModestKicker05PoisedToKick_svg from '../../images/africaModest/africaModestKicker05PoisedToKick_svg.js';
+import africaModestKicker05Kicking_svg from '../../images/africaModest/africaModestKicker05Kicking_svg.js';
+import africaModestKicker07Standing_svg from '../../images/africaModest/africaModestKicker07Standing_svg.js';
+import africaModestKicker07PoisedToKick_svg from '../../images/africaModest/africaModestKicker07PoisedToKick_svg.js';
+import africaModestKicker07Kicking_svg from '../../images/africaModest/africaModestKicker07Kicking_svg.js';
+import africaModestKicker11Standing_svg from '../../images/africaModest/africaModestKicker11Standing_svg.js';
+import africaModestKicker11PoisedToKick_svg from '../../images/africaModest/africaModestKicker11PoisedToKick_svg.js';
+import africaModestKicker11Kicking_svg from '../../images/africaModest/africaModestKicker11Kicking_svg.js';
+import africaModestKicker13Standing_svg from '../../images/africaModest/africaModestKicker13Standing_svg.js';
+import africaModestKicker13PoisedToKick_svg from '../../images/africaModest/africaModestKicker13PoisedToKick_svg.js';
+import africaModestKicker13Kicking_svg from '../../images/africaModest/africaModestKicker13Kicking_svg.js';
 
 /**
  * Artwork representing characters from Africa (modest).
@@ -24,33 +39,58 @@ const KickerPortrayalAfricaModest = new KickerPortrayal(
 
   // Unnumbered Kickers
   [
-    KickerPortrayalAfrica.unnumberedKickerImages[ 1 - 1 ],
-    KickerPortrayalAfrica.unnumberedKickerImages[ 4 - 1 ],
     {
-      standing: africaModestKicker03Standing_png,
-      poisedToKick: africaModestKicker03PoisedToKick_png,
-      kicking: africaModestKicker03Kicking_png
+      standing: africaModestKicker01Standing_svg,
+      poisedToKick: africaModestKicker01PoisedToKick_svg,
+      kicking: africaModestKicker01Kicking_svg
     },
-    KickerPortrayalUSA.unnumberedKickerImages[ 14 - 1 ],
-
-    KickerPortrayalAfrica.unnumberedKickerImages[ 3 - 1 ],
     {
-      standing: africaModestKicker06Standing_png,
-      poisedToKick: africaModestKicker06PoisedToKick_png,
-      kicking: africaModestKicker06Kicking_png
+      standing: africaModestKicker02Standing_svg,
+      poisedToKick: africaModestKicker02PoisedToKick_svg,
+      kicking: africaModestKicker02Kicking_svg
+    },
+    {
+      standing: africaModestKicker03Standing_svg,
+      poisedToKick: africaModestKicker03PoisedToKick_svg,
+      kicking: africaModestKicker03Kicking_svg
     },
     KickerPortrayalAfrica.unnumberedKickerImages[ 7 - 1 ],
+    {
+      standing: africaModestKicker05Standing_svg,
+      poisedToKick: africaModestKicker05PoisedToKick_svg,
+      kicking: africaModestKicker05Kicking_svg
+    },
     KickerPortrayalUSA.unnumberedKickerImages[ 8 - 1 ],
+    {
+      standing: africaModestKicker07Standing_svg,
+      poisedToKick: africaModestKicker07PoisedToKick_svg,
+      kicking: africaModestKicker07Kicking_svg
+    },
+    KickerPortrayalAfrica.unnumberedKickerImages[ 14 - 1 ],
+    KickerPortrayalAfrica.unnumberedKickerImages[ 13 - 1 ],
     KickerPortrayalUSA.unnumberedKickerImages[ 2 - 1 ],
-    KickerPortrayalAfrica.unnumberedKickerImages[ 11 - 1 ] ],
+    {
+      standing: africaModestKicker11Standing_svg,
+      poisedToKick: africaModestKicker11PoisedToKick_svg,
+      kicking: africaModestKicker11Kicking_svg
+    },
+    KickerPortrayalAfrica.unnumberedKickerImages[ 1 - 1 ],
+    {
+      standing: africaModestKicker13Standing_svg,
+      poisedToKick: africaModestKicker13PoisedToKick_svg,
+      kicking: africaModestKicker13Kicking_svg
+    },
+    KickerPortrayalAfrica.unnumberedKickerImages[ 4 - 1 ],
+    KickerPortrayalAfrica.unnumberedKickerImages[ 15 - 1 ]
+  ],
 
   // Numbered Kickers
   [
     KickerPortrayalAfrica.numberedKickerImages[ 1 - 1 ],
     {
-      standing: africaModestVariabilityKicker02Standing_png,
-      poisedToKick: africaModestVariabilityKicker02PoisedToKick_png,
-      kicking: africaModestVariabilityKicker02Kicking_png
+      standing: africaModestVariabilityKicker02Standing_svg,
+      poisedToKick: africaModestVariabilityKicker02PoisedToKick_svg,
+      kicking: africaModestVariabilityKicker02Kicking_svg
     },
     KickerPortrayalAfrica.numberedKickerImages[ 3 - 1 ],
     KickerPortrayalAfrica.numberedKickerImages[ 4 - 1 ]
