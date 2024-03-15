59db8c03d82f9047d53a358834308e8335b976ed
Marla Schulz <marla.schulz@colorado.edu>
2024-03-15 15:32:18 -0600

    Add no kicks message, see: https://github.com/phetsims/mean-share-and-balance/issues/154

---
+++ js/SoccerCommonStrings.ts
@@ -0,0 +1,19 @@
+// Copyright 2023, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import type LocalizedStringProperty from '../../chipper/js/LocalizedStringProperty.js';
+import soccerCommon from './soccerCommon.js';
+
+type StringsType = {
+  'needAtLeastOneKickStringProperty': LocalizedStringProperty;
+};
+
+const SoccerCommonStrings = getStringModule( 'SOCCER_COMMON' ) as StringsType;
+
+soccerCommon.register( 'SoccerCommonStrings', SoccerCommonStrings );
+
+export default SoccerCommonStrings;
