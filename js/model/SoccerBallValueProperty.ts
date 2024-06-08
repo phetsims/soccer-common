edbd20af4b64ad1e38707107226fa527bedb6722
phet-dev <phetadmin@gmail.com>
2024-06-08 03:19:49 -0600

    update copyright dates from daily grunt work

--- js/model/SoccerBallValueProperty.ts
+++ js/model/SoccerBallValueProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * This Property represents the value of a soccer ball within a soccer scene. The value can either be a number or null.
